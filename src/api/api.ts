8c119a4b2723c05339ce60e0718df8768ae99646
Axel Munoz <naclif30@gmail.com>
2025-09-27 01:56:16 -0300

    DEPLOY BITCHES

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://gastosapp-nestjs.onrender.com', // http://localhost:3000
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
